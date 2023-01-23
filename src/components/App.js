e43710bd246c2f56ba5f2fd341ac37347445de46
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-01-23 22:15:12 +0300

    restructure folder hierachy and project presentation

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from "./home/Home";
+import OurServices from "./ourservices/OurServices";
+import Gallery from "./gallery/Gallery";
+import ContactUs from "./contactus/ContactUs";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      {/* <OurServices />
+      <Gallery />
+      <ContactUs /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar(){
+    return (
+        <>
+            <h3>FLEXY GYM</h3>
+        </>
+    )
+}
+
+export default NavBar;
